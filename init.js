07c20b45a86bef0fa0675cb5ece0598ff8ebfdf8
3spectrummessage3 <53975904+3spectrummessage3@users.noreply.github.com>
2019-08-10 23:44:05 +0700

    Add files via upload

---
+++ init.js
@@ -0,0 +1,258 @@
+$(document).ready(function () {
+  var count = 1;
+  var s = "";
+  $(".btn1").click(function() {
+    s +="R";
+    switch (count) {
+      case 1:
+        $("#box2").css("background-color","rgb(255, 0, 0)");
+        count++;
+        break;
+      case 2:
+        $("#box3").css("background-color","rgb(255, 0, 0)");
+        count++;
+        break;
+      case 3:
+        $("#box4").css("background-color","rgb(255, 0, 0)");
+        count++;
+        break;
+      case 4:
+        $("#box5").css("background-color","rgb(255, 0, 0)");
+        count++;
+        break;
+      case 5:
+       $("#box6").css("background-color","rgb(255, 0, 0)");
+        count++;
+        break;
+      case 6:
+        $("#box7").css("background-color","rgb(255, 0, 0)");
+        count++;
+        break;
+      case 7:
+        $("#box8").css("background-color","rgb(255, 0, 0)");
+        break;
+      default:
+    }
+  })
+  $(".btn2").click(function() {
+    s +="O";
+    switch (count) {
+      case 1:
+        $("#box2").css("background-color","rgb(255, 92, 0)");
+        count++;
+        break;
+      case 2:
+        $("#box3").css("background-color","rgb(255, 92, 0)");
+        count++;
+        break;
+      case 3:
+        $("#box4").css("background-color","rgb(255, 92, 0)");
+        count++;
+        break;
+      case 4:
+        $("#box5").css("background-color","rgb(255, 92, 0)");
+        count++;
+        break;
+      case 5:
+       $("#box6").css("background-color","rgb(255, 92, 0)");
+        count++;
+        break;
+      case 6:
+        $("#box7").css("background-color","rgb(255, 92, 0)");
+        count++;
+        break;
+      case 7:
+        $("#box8").css("background-color","rgb(255, 92, 0)");
+        break;
+      default:
+    }
+  })
+
+  $(".btn3").click(function() {
+    s +="Y";
+    switch (count) {
+      case 1:
+        $("#box2").css("background-color","rgb(255, 245, 0)");
+        count++;
+        break;
+      case 2:
+        $("#box3").css("background-color","rgb(255, 245, 0)");
+        count++;
+        break;
+      case 3:
+        $("#box4").css("background-color","rgb(255, 245, 0)");
+        count++;
+        break;
+      case 4:
+        $("#box5").css("background-color","rgb(255, 245, 0)");
+        count++;
+        break;
+      case 5:
+       $("#box6").css("background-color","rgb(255, 245, 0)");
+        count++;
+        break;
+      case 6:
+        $("#box7").css("background-color","rgb(255, 245, 0)");
+        count++;
+        break;
+      case 7:
+        $("#box8").css("background-color","rgb(255, 245, 0)");
+        break;
+      default:
+    }
+  })
+  $(".btn4").click(function() {
+    s +="G";
+    switch (count) {
+      case 1:
+        $("#box2").css("background-color","rgb(0, 242, 2)");
+        count++;
+        break;
+      case 2:
+        $("#box3").css("background-color","rgb(0, 242, 2)");
+        count++;
+        break;
+      case 3:
+        $("#box4").css("background-color","rgb(0, 242, 2)");
+        count++;
+        break;
+      case 4:
+        $("#box5").css("background-color","rgb(0, 242, 2)");
+        count++;
+        break;
+      case 5:
+       $("#box6").css("background-color","rgb(0, 242, 2)");
+        count++;
+        break;
+      case 6:
+        $("#box7").css("background-color","rgb(0, 242, 2)");
+        count++;
+        break;
+      case 7:
+        $("#box8").css("background-color","rgb(0, 242, 2)");
+        break;
+      default:
+    }
+  })
+  $(".btn5").click(function() {
+    s +="L";
+    switch (count) {
+      case 1:
+        $("#box2").css("background-color","rgb(0, 255, 255)");
+        count++;
+        break;
+      case 2:
+        $("#box3").css("background-color","rgb(0, 255, 255)");
+        count++;
+        break;
+      case 3:
+        $("#box4").css("background-color","rgb(0, 255, 255)");
+        count++;
+        break;
+      case 4:
+        $("#box5").css("background-color","rgb(0, 255, 255)");
+        count++;
+        break;
+      case 5:
+       $("#box6").css("background-color","rgb(0, 255, 255)");
+        count++;
+        break;
+      case 6:
+        $("#box7").css("background-color","rgb(0, 255, 255)");
+        count++;
+        break;
+      case 7:
+        $("#box8").css("background-color","rgb(0, 255, 255)");
+        break;
+      default:
+    }
+  })
+  $(".btn6").click(function() {
+    s +="B";
+    switch (count) {
+      case 1:
+        $("#box2").css("background-color","rgb(0, 10, 255)");
+        count++;
+        break;
+      case 2:
+        $("#box3").css("background-color","rgb(0, 10, 255)");
+        count++;
+        break;
+      case 3:
+        $("#box4").css("background-color","rgb(0, 10, 255)");
+        count++;
+        break;
+      case 4:
+        $("#box5").css("background-color","rgb(0, 10, 255)");
+        count++;
+        break;
+      case 5:
+       $("#box6").css("background-color","rgb(0, 10, 255)");
+        count++;
+        break;
+      case 6:
+        $("#box7").css("background-color","rgb(0, 10, 255)");
+        count++;
+        break;
+      case 7:
+        $("#box8").css("background-color","rgb(0, 10, 255)");
+        break;
+      default:
+    }
+
+  })
+  $(".btn7").click(function() {
+    s +="V";
+    switch (count) {
+      case 1:
+        $("#box2").css("background-color","rgb(135, 0, 255)");
+        count++;
+        break;
+      case 2:
+        $("#box3").css("background-color","rgb(135, 0, 255)");
+        count++;
+        break;
+      case 3:
+        $("#box4").css("background-color","rgb(135, 0, 255)");
+        count++;
+        break;
+      case 4:
+        $("#box5").css("background-color","rgb(135, 0, 255)");
+        count++;
+        break;
+      case 5:
+       $("#box6").css("background-color","rgb(135, 0, 255)");
+        count++;
+        break;
+      case 6:
+        $("#box7").css("background-color","rgb(135, 0, 255)");
+        count++;
+        break;
+      case 7:
+        $("#box8").css("background-color","rgb(135, 0, 255)");
+        break;
+      default:
+    }
+  })
+  $(".btn8").click(function() {
+    s ="";
+    $("#box2").css("background-color","rgb(34, 33, 37)");
+    $("#box3").css("background-color","rgb(34, 33, 37)");
+    $("#box4").css("background-color","rgb(34, 33, 37)");
+    $("#box5").css("background-color","rgb(34, 33, 37)");
+    $("#box6").css("background-color","rgb(34, 33, 37)");
+    $("#box7").css("background-color","rgb(34, 33, 37)");
+    $("#box8").css("background-color","rgb(34, 33, 37)");
+    count = 1;
+  })
+  $(".btn9").click(function() {
+      if (s == "RBYLGOV") {
+        alert("Молодец, ты дошёл до конца этого небольшого квеста! Обязательно напиши мне в лс, что ты молодец, чтобы я лично это подтвердил ;)")
+      }
+      else {
+        // $(".message").css("z-index","11");
+        // $(".all").css("z-index","11");
+        alert("Слыш, ты чо неправильный код вводишь? Пиздим его ребят! ")
+      }
+  })
+})
